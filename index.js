62e77715bdfdb500d579a4e383431f0b0cfc2335
Mithilesh <mithileshmarekar01@gmail.com>
2023-06-26 10:17:38 +0530

    add a controller for home

---
+++ index.js
@@ -0,0 +1,23 @@
+//Requiring Express
+const express = require('express');
+const app = express();
+//Port No
+const port = 8000;
+
+
+//use express router
+app.use('/', require('./routes')); // goes to routes folder to access routes
+
+
+
+
+//binding to port no and listning for any connections .
+app.listen(port, function(err){
+    // if error in connection , print this on console.
+    if (err) {  
+        // using `  ` to print on console. it is called INTERPOLATION Method.
+      console.log(`Error in Running Server :${err}`);
+    }
+    //if Connection successfull, print this on console.
+    console.log(`Server is Running on Port : ${port}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+//This express gets loaded automaticall with the main Index.js file 
+// as both require express. so as the main index.js gets loaded it saves
+//  the instance of express that can be used anywhere.
+
+const express = require('express'); 
+const homeController = require('../controllers/home_controller');
+const router = express.Router();
+console.log("Router Activated");
+
+router.get('/',homeController.home );
+
+module.exports = router;
+
