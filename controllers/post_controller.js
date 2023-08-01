9d1eeb0eda40fe44d57c038f7ee6953a8cfd839d
Mithilesh <mithileshmarekar01@gmail.com>
2023-08-01 18:20:47 +0530

    Added Post and user ejs and rendered them using controllers

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,3 +1,8 @@
 module.exports.post = (req,res) =>{
-    return res.end('<h1>Post Controller</h1>');
+   // return res.end('<h1>Post Controller</h1>');
+    
+   //rendering post.ejs
+   return res.render('post', {
+    title: "POst"  // this title will appear in <%= title %> of home.ejs
+});
 }
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,8 @@
 module.exports.profile = (req, res) =>{
-  return  res.end('<h1>User Profile Controller</h1>');
+  // return  res.end('<h1>User Profile Controller</h1>');
+  
+  //rendering user.ejs
+  return res.render('user', {
+    title: "USER"  // this title will appear in <%= title %> of home.ejs
+});
 }
