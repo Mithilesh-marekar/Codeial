9ec6757ad178e1345fe6038fd3e5c3baff2bac68
Mithilesh <mithileshmarekar01@gmail.com>
2023-08-01 18:14:04 +0530

    Created views for home cntroller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,13 @@
 module.exports.home = (req, res) =>{
-    return res.end('<h1>Express is up for codial !!!</h1>');
+    //this line is responding directly to browser
+    // return res.end('<h1>Express is up for codial !!!</h1>');
+    
+    //rendering the home.ejs file
+    return res.render('home', {
+        title: "Home"  // this title will appear in <%= title %> of home.ejs
+    });
 }
+
 
 
 //creating Controller.
