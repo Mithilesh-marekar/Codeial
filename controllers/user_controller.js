392585d8c549786348984e76cd2955f13f8369d4
Mithilesh <mithileshmarekar01@gmail.com>
2023-09-07 17:45:21 +0530

    Created views for sign up and sign in page. Addec controllers to control Actions. Added routes for the controller to fetch and render the views.

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,6 @@
+
+// Controller Action's for Rendering Pages
+
 module.exports.profile = (req, res) =>{
   // return  res.end('<h1>User Profile Controller</h1>');
   
@@ -6,3 +9,17 @@ module.exports.profile = (req, res) =>{
     title: "USER"  // this title will appear in <%= title %> of home.ejs
 });
 }
+
+//render the sign up page
+module.exports.signUp =(req, res) =>{
+  return res.render('user_sign_up',{
+    title: " Codeial | Sign Up",
+  })
+}
+//render the sign in page
+module.exports.signIn = (req, res) =>{
+  return res.render('user_sign_In',{
+    title: "Coedeial | Sign In",
+  })
+}
+
--- routes/users_route.js
+++ routes/users_route.js
@@ -8,7 +8,9 @@ const usersController = require('../controllers/user_controller');
 //getting the action 'i.e profile' exported by users_controller
 router.get('/profile', usersController.profile );
 
-
+// Getting the Actions exported by Users controller for Sign In and Sign Up.
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 
 //exporting router
 module.exports = router;
